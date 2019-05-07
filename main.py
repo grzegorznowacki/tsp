8de112128e8928ff693c0a4363f9cd53ad14a138
Grzegorz Nowacki <grzegorznowack@gmail.com>
2019-05-07 12:25:46 +0200

    Remove path output

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ starting_point2 = utils.draw_starting_point(points_list)
 (res1, tabu_list) = algorithms.nearest_neighbour_alg_for_first_path(starting_point1, points_list, point_index_dict)
 res2 = algorithms.nearest_neighbour_alg_for_second_path(starting_point2, points_list, point_index_dict, tabu_list)
 
-print(res1)
-print(res2)
+print(res1[0])
+print(res2[0])
 
 utils.save_paths_to_file(res1[1], res2[1], OUTPUT_FILE_PATH)
 
@@ -30,8 +30,8 @@ perm_res2 = algorithms.permutations_fix_for_second_path(starting_point2, points_
 
 utils.save_paths_to_file(perm_res1[1], perm_res2[1], OUTPUT_FILE_PATH_PERMUTATIONS)
 
-print(perm_res1)
-print(perm_res2)
+print(perm_res1[0])
+print(perm_res2[0])
 
 print(res1[1] == perm_res1[1])
 
